b22ad82e5b8f3e461b5d9fdc9960aca0d08bdb2c
aaronwlee <aaron.lee@cannalogue.ca>
2020-06-03 14:45:12 -0400

    fix(bug) application

--- application.ts
+++ application.ts
@@ -64,7 +64,7 @@ export class App extends Router {
     }
   }
 
-  listen = (
+  listen = async (
     { port, secure, keyFile, certFile, hostname = "0.0.0.0", debug = false }:
       ListenProps,
   ) => {
