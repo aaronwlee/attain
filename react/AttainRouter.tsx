3a90484a45dd36e6cc33c02700bafe1c761d7f54
aaronwlee <aaron.lee@cannalogue.ca>
2020-07-23 09:10:31 -0400

    feat(react): add react component

--- react/AttainRouter.tsx
+++ react/AttainRouter.tsx
@@ -1,7 +1,6 @@
 // @deno-types="https://deno.land/x/types/react/v16.13.1/react.d.ts"
 import React from 'https://jspm.dev/react@16.13.1';
 
-// @ts-ignore
 const RouterContext = React.createContext<any>({
   pathname: "/",
 });
