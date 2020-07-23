fd92b2260f253b8b86eb1b2022cd91b7210f9ad0
aaronwlee <aaron.lee@cannalogue.ca>
2020-07-23 08:15:46 -0400

    fix(version)

--- mod.ts
+++ mod.ts
@@ -1,8 +1,6 @@
 export { App } from "./application.ts";
 export { Router } from "./router.ts";
 
-export { SupportMethodType } from "./types.ts";
-
 export { Request } from "./request.ts";
 export { Response } from "./response.ts";
 
