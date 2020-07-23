fc731664caa60a1c4d2b78bf77a54300ae6be840
aaronwlee <aaron.lee@cannalogue.ca>
2020-07-23 13:27:48 -0400

    feat(react): head controller added

--- react/AttainReactUtils.js
+++ react/AttainReactUtils.js
@@ -1,10 +1,13 @@
+// @deno-types="https://deno.land/x/types/react/v16.13.1/react.d.ts"
+import React from "https://jspm.dev/react@16.13.1";
+
 /**
  * required to use "lib": ["dom", "dom.iterable", "esnext", "deno.ns", "deno.unstable"]
  */
 export const useDocument = () => {
   try {
     //@ts-ignore
-    if(document) {
+    if (document) {
       return document;
     } else {
       return undefined;
@@ -14,19 +17,87 @@ export const useDocument = () => {
   }
 };
 
-const head = [];
+let header = [];
+const dom = useDocument();
 
 
-export function addToHead(data) {
-  const dom = useDocument();
+/**
+ * 
+ * @param {string} name 
+ * @param {object} attr 
+ */
+export const addMeta = (name, attr) => {
+  if (dom) {
+    const meta = dom.querySelector(`meta[name="${name}"]`);
 
-  if(dom) {
-    dom.head.innerHTML += data;
+    if (meta) {
+      Object.keys(attr).forEach(key => {
+        meta[key] = attr[key];
+      });
+    } else {
+      const newMeta = dom.createElement("meta");
+      Object.keys(attr).forEach(key => {
+        newMeta[key] = attr[key];
+      });
+      dom.getElementsByTagName("head")[0].appendChild(meta);
+    }
   } else {
-    head.push(data)
+    header.push( /*#__PURE__*/React.createElement("meta", attr));
   }
-}
+};
+
+
+/**
+ * 
+ * @param {object} attr 
+ * @param {string} text 
+ */
+export const addScript = (attr, text) => {
+  if (dom) {
+    const newScript = dom.createElement("script");
+    Object.keys(attr).forEach(key => {
+      newScript[key] = attr[key];
+    });
+    newScript["text"] = text;
+    dom.getElementsByTagName("head")[0].appendChild(newScript);
+  } else {
+    header.push( /*#__PURE__*/React.createElement("script", _extends({}, attr, {
+      dangerouslySetInnerHTML: {
+        __html: text
+      }
+    })));
+  }
+};
+
+
+/**
+ * 
+ * @param {string} text 
+ */
+export const setTitle = text => {
+  if (dom) {
+    const title = dom.querySelector("title");
+
+    if (title) {
+      title["text"] = text;
+    } else {
+      const newTitle = dom.createElement("title");
+      newTitle["text"] = text;
+      dom.getElementsByTagName("head")[0].appendChild(newTitle);
+    }
+  } else {
+    header.push( /*#__PURE__*/React.createElement("title", {
+      dangerouslySetInnerHTML: {
+        __html: text
+      }
+    }));
+  }
+};
 
 export function getHead() {
-  return head.join("\n");
+  try {
+    return header;
+  } finally {
+    header = [];
+  }
 }
