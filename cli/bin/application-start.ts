e460455968f6645716641596c2b933734e7ddc3f
aaronwlee <aaron.lee@cannalogue.ca>
2020-07-23 23:15:51 -0400

    fix(denon)

--- cli/bin/application-start.ts
+++ cli/bin/application-start.ts
@@ -3,7 +3,6 @@ import { red } from "../../deps.ts";
 export async function startServer(envState: "dev" | "start") {
   const env = envState === "dev" ? "DEVElOPMENT" : "PRODUCTION"
   try {
-    const currentPath = Deno.cwd();
     const process = Deno.run({
       env: {
         [env]: "true",
@@ -13,7 +12,8 @@ export async function startServer(envState: "dev" | "start") {
         "run",
         "-A",
         "--unstable",
-        `${currentPath}/server.tsx`,
+        "https://deno.land/x/denon@v2.2.1/denon.ts",
+        "start"
       ],
     });
 
