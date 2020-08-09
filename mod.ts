cb98196fdf78baa7d94f112233be5aa62b463cdd
itays123 <itays2005@gmail.com>
2020-08-09 15:30:49 +0300

    added to exports

--- mod.ts
+++ mod.ts
@@ -1,3 +1,5 @@
+import { f } from "../../../../../AppData/Local/deno/deps/https/jspm.dev/1b6a1e6c7f1d37e8ae4d41f357139075be43391fcfef23407ab19863891833c1.ts";
+
 export { App } from "./core/application.ts";
 export { Router } from "./core/router.ts";
 
@@ -9,5 +11,7 @@ export { logger } from "./plugins/logger.ts";
 export { staticServe } from "./plugins/staticServe.ts";
 export { security } from "./plugins/security.ts";
 export { ViewEngine } from "./plugins/ViewEngine.ts";
+
+export { AttainDatabase } from "./core/database.ts"
 
 export * from "./core/types.ts";
