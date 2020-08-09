4366eced5c6a9fca19ec2b6f88b72902e98b1df6
itays123 <itays2005@gmail.com>
2020-08-09 15:32:20 +0300

    vscode autocomplete added some import so I deleted it

--- mod.ts
+++ mod.ts
@@ -1,5 +1,3 @@
-import { f } from "../../../../../AppData/Local/deno/deps/https/jspm.dev/1b6a1e6c7f1d37e8ae4d41f357139075be43391fcfef23407ab19863891833c1.ts";
-
 export { App } from "./core/application.ts";
 export { Router } from "./core/router.ts";
 
