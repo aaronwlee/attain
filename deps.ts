68fbc9e1e7d7f137c6de573af5cf859e4c2ee00c
Wooseok Lee <wooseoklee@wooseoks-mbp.lan>
2020-09-19 08:14:54 -0400

    upgrade: use std 0.70 (aaronwlee/attain#36)

--- deps.ts
+++ deps.ts
@@ -1,12 +1,19 @@
 export {
   serve,
   serveTLS,
+} from "https://deno.land/std@0.70.0/http/server.ts";
+
+export type {
   ServerRequest,
   Server,
-  Response
-} from "https://deno.land/std@0.65.0/http/server.ts";
-export { Status, STATUS_TEXT } from "https://deno.land/std@0.65.0/http/http_status.ts";
+  Response,
+} from "https://deno.land/std@0.70.0/http/server.ts";
 
+
+export {
+  Status,
+  STATUS_TEXT,
+} from "https://deno.land/std@0.70.0/http/http_status.ts";
 
 export {
   contentType,
@@ -16,12 +23,13 @@ export {
 
 export {
   deferred,
-  Deferred
-} from "https://deno.land/std@0.65.0/async/mod.ts";
+} from "https://deno.land/std@0.70.0/async/mod.ts";
+
+export type { Deferred } from "https://deno.land/std@0.70.0/async/mod.ts";
 
 export {
   Sha1,
-} from "https://deno.land/std@0.65.0/hash/sha1.ts";
+} from "https://deno.land/std@0.70.0/hash/sha1.ts";
 
 export {
   pathToRegexp,
@@ -36,22 +44,29 @@ export {
   bold,
   blue,
   magenta,
-} from "https://deno.land/std@0.65.0/fmt/colors.ts";
+} from "https://deno.land/std@0.70.0/fmt/colors.ts";
 
 export { default as isEmpty } from "https://raw.githubusercontent.com/lodash/lodash/master/isEmpty.js";
 
-export { extname } from "https://deno.land/std@0.65.0/path/mod.ts";
+export { extname } from "https://deno.land/std@0.70.0/path/mod.ts";
 
-export { parse } from "https://deno.land/std@0.65.0/flags/mod.ts";
-export { ensureDir } from "https://deno.land/std@0.65.0/fs/mod.ts";
-export { EventEmitter } from "https://deno.land/std@0.65.0/node/events.ts";
-export { listenAndServe } from "https://deno.land/std@0.65.0/http/server.ts";
-export { acceptWebSocket, acceptable, isWebSocketCloseEvent } from "https://deno.land/std@0.65.0/ws/mod.ts";
-export { parse as yamlParse, stringify as yamlStringify } from "https://deno.land/std@0.65.0/encoding/yaml.ts";
+export { parse } from "https://deno.land/std@0.70.0/flags/mod.ts";
+export { ensureDir } from "https://deno.land/std@0.70.0/fs/mod.ts";
+export { EventEmitter } from "https://deno.land/std@0.70.0/node/events.ts";
+export { listenAndServe } from "https://deno.land/std@0.70.0/http/server.ts";
+export {
+  acceptWebSocket,
+  acceptable,
+  isWebSocketCloseEvent,
+} from "https://deno.land/std@0.70.0/ws/mod.ts";
+export {
+  parse as yamlParse,
+  stringify as yamlStringify,
+} from "https://deno.land/std@0.70.0/encoding/yaml.ts";
 
-export { assert } from "https://deno.land/std@0.65.0/testing/asserts.ts";
+export { assert } from "https://deno.land/std@0.70.0/testing/asserts.ts";
 
-export { copyBytes, equal } from "https://deno.land/std@0.65.0/bytes/mod.ts";
+export { copyBytes, equal } from "https://deno.land/std@0.70.0/bytes/mod.ts";
 export {
   posix,
-} from "https://deno.land/std@0.65.0/path/mod.ts";
+} from "https://deno.land/std@0.70.0/path/mod.ts";
